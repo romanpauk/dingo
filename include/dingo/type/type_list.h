ae50f851331e38fc4ea62fe40c4ee0197c7e4f1e
Roman Paukner <romanpauk@gmail.com>
2026-03-30 15:34:01 +0200

    Simplify type_list iteration (#54)

--- include/dingo/type/type_list.h
+++ include/dingo/type/type_list.h
@@ -9,6 +9,12 @@
 
 #include <dingo/config.h>
 
+#include <cstddef>
+#include <initializer_list>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
 namespace dingo {
 template <typename... Types> struct type_list {};
 template <typename T> struct type_list_iterator {
@@ -37,29 +43,20 @@ struct type_list_size : std::integral_constant<size_t, sizeof...(Types)> {};
 template <typename... Types>
 static constexpr size_t type_list_size_v = type_list_size<Types...>::value;
 
-template <typename RTTI, typename Function>
-bool for_type(type_list<>, const typename RTTI::type_index&, Function&&) {
-    return false;
-}
-
-template <typename RTTI, typename Function, typename Head, typename... Tail>
-bool for_type(type_list<Head, Tail...>, const typename RTTI::type_index& type,
+template <typename RTTI, typename Function, typename... Types>
+bool for_type(type_list<Types...>, const typename RTTI::type_index& type,
               Function&& fn) {
-    if (RTTI::template get_type_index<Head>() == type) {
-        fn(type_list_iterator<Head>());
-        return true;
-    } else {
-        return for_type<RTTI>(type_list<Tail...>{}, type,
-                              std::forward<Function>(fn));
-    }
+    bool matched = false;
+    (void)std::initializer_list<int>{
+        (((!matched && RTTI::template get_type_index<Types>() == type)
+              ? (fn(type_list_iterator<Types>{}), matched = true, 0)
+              : 0))...};
+    return matched;
 }
 
-template <typename Function> void for_each(type_list<>, Function&&) {}
-
-template <typename Function, typename Head, typename... Tail>
-void for_each(type_list<Head, Tail...>, Function&& fn) {
-    fn(type_list_iterator<Head>());
-    for_each(type_list<Tail...>{}, std::forward<Function>(fn));
+template <typename Function, typename... Types>
+void for_each(type_list<Types...>, Function&& fn) {
+    (fn(type_list_iterator<Types>{}), ...);
 }
 
 template <typename Tuple, size_t I, typename Arg, typename Index>
