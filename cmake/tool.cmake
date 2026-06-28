5886ddffc02f40c91b322bb8249a54aee719e726
Roman Paukner <romanpauk@gmail.com>
2026-06-28 23:12:19 +0200

    Use clang-tidy and clang-format from uv en (#116)

--- cmake/tool.cmake
+++ cmake/tool.cmake
@@ -2,32 +2,48 @@ function(find_tool out_executable out_version_output out_error)
     cmake_parse_arguments(
         TOOL
         ""
-        "NAME;VERSION;VERSION_MATCH"
-        "NAMES"
+        "NAME"
+        ""
         ${ARGN}
     )
 
     if(NOT TOOL_NAME)
         message(FATAL_ERROR "find_tool requires NAME")
     endif()
-    if(NOT TOOL_VERSION)
-        message(FATAL_ERROR "find_tool requires VERSION")
-    endif()
-    if(NOT TOOL_VERSION_MATCH)
-        message(FATAL_ERROR "find_tool requires VERSION_MATCH")
-    endif()
-    if(NOT TOOL_NAMES)
-        set(TOOL_NAMES
-            ${TOOL_NAME}-${TOOL_VERSION}
-            ${TOOL_NAME}
-        )
-    endif()
 
-    find_program(${out_executable}
-        NAMES ${TOOL_NAMES}
-        DOC "${TOOL_NAME} executable matching version ${TOOL_VERSION}"
-    )
-    set(TOOL_EXECUTABLE "${${out_executable}}")
+    find_program(TOOL_UV_EXE NAMES uv)
+    if(NOT TOOL_UV_EXE)
+        string(CONCAT TOOL_ERROR
+            "${TOOL_NAME} is required; uv was not found"
+        )
+    elseif(NOT EXISTS "${PROJECT_SOURCE_DIR}/uv.lock")
+        string(CONCAT TOOL_ERROR
+            "${TOOL_NAME} is required; "
+            "${PROJECT_SOURCE_DIR}/uv.lock was not found"
+        )
+    else()
+        execute_process(
+            COMMAND ${TOOL_UV_EXE} run --locked python -c
+                "import shutil; print(shutil.which('${TOOL_NAME}') or '')"
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+            OUTPUT_VARIABLE TOOL_UV_OUTPUT
+            ERROR_VARIABLE TOOL_UV_ERROR
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_STRIP_TRAILING_WHITESPACE
+            RESULT_VARIABLE TOOL_UV_RESULT
+        )
+        if(TOOL_UV_RESULT EQUAL 0 AND TOOL_UV_OUTPUT)
+            set(TOOL_EXECUTABLE "${TOOL_UV_OUTPUT}")
+            set(${out_executable} "${TOOL_EXECUTABLE}" CACHE FILEPATH
+                "${TOOL_NAME} executable from uv.lock"
+                FORCE)
+        else()
+            string(CONCAT TOOL_ERROR
+                "${TOOL_NAME} is required; "
+                "uv locked environment did not provide ${TOOL_NAME}"
+            )
+        endif()
+    endif()
 
     if(TOOL_EXECUTABLE)
         execute_process(
@@ -42,22 +58,17 @@ function(find_tool out_executable out_version_output out_error)
             "${TOOL_VERSION_OUTPUT}"
         )
 
-        if(NOT TOOL_VERSION_RESULT EQUAL 0
-                OR NOT TOOL_VERSION_OUTPUT MATCHES "${TOOL_VERSION_MATCH}")
+        if(NOT TOOL_VERSION_RESULT EQUAL 0)
             string(CONCAT TOOL_ERROR
-                "${TOOL_NAME}-${TOOL_VERSION} is required; "
-                "found '${TOOL_VERSION_OUTPUT}' at "
+                "${TOOL_NAME} is required; "
+                "'${TOOL_NAME} --version' failed at "
                 "'${TOOL_EXECUTABLE}'"
             )
             set(TOOL_EXECUTABLE "")
             set(${out_executable} "${out_executable}-NOTFOUND" CACHE FILEPATH
-                "${TOOL_NAME} executable matching version ${TOOL_VERSION}"
+                "${TOOL_NAME} executable from uv.lock"
                 FORCE)
         endif()
-    else()
-        string(CONCAT TOOL_ERROR
-            "${TOOL_NAME}-${TOOL_VERSION} is required"
-        )
     endif()
 
     set(${out_executable} "${TOOL_EXECUTABLE}" PARENT_SCOPE)
