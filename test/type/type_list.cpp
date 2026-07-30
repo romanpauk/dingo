53952c7a9bafaf2dfd6d7aca2cb731794c68c25c
Roman Paukner <romanpauk@gmail.com>
2026-07-30 13:32:02 +0200

    Optimize type-list deduplication and pointer qualification expansion (#152)

--- test/type/type_list.cpp
+++ test/type/type_list.cpp
@@ -35,8 +35,10 @@ TEST(type_list_test, meta_utilities) {
   static_assert(type_list_size_v<nested_list> == 2);
   static_assert(std::is_same_v<type_list_head_t<nested_list>, type_list_a>);
   static_assert(std::is_same_v<to_type_list_t<nested_tuple>, nested_list>);
+  static_assert(!type_list_contains_v<type_list_a, type_list<>>);
   static_assert(type_list_contains_v<type_list_a, nested_list>);
   static_assert(!type_list_contains_v<type_list_c, nested_list>);
+  static_assert(std::is_same_v<type_list_unique_t<type_list<>>, type_list<>>);
   static_assert(
       std::is_same_v<
           type_list_unique_t<type_list<type_list_a, type_list_b, type_list_a,
